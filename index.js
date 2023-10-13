b94e63524b587486573b44216961a0b13caf305e
senilm <senilmendapara.it20@scet.ac.in>
2023-10-13 14:02:37 +0530

    vercel change

--- index.js
+++ index.js
@@ -40,6 +40,19 @@ const corsOptions ={
  }
  
 app.use(cors(corsOptions))
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+    );
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    if (req.method === "OPTIONS") {
+      return res.sendStatus(200);
+    }
+    next();
+  });
+
 app.use('/assets', express.static(path.join(__dirname,"public/assets")))
 
 
